eec5fc9c20964bedf3c6b38638778736a8464853
Jade1724 <haruka.i1724@gmail.com>
2022-09-11 11:40:17 +1200

    club detecting seal

---
+++ Scripts/GamePlay/Seal.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func play_tone():
+	get_tree().root.get_node("Game/AudioStreamPlayer").play()
+
+func _on_Area_area_entered(area):
+	#play_tone()
+	if area.get_parent().get_groups().has("weapon"):
+		play_tone()
+	pass # Replace with function body.
