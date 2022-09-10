21e3446424f3e196ab3762ca945848efd03757b5
Jade1724 <haruka.i1724@gmail.com>
2022-09-10 18:29:02 +1200

    Added StartGameCountDownTimer and GamePlayTimer in GamePlay scene

--- Scripts/GamePlay/GamePlay.gd
+++ Scripts/GamePlay/GamePlay.gd
@@ -1,10 +1,24 @@
 extends Spatial
 
+onready var start_game_count_down_timer = $StartGameCountDownTimer
+onready var game_play_timer = $GamePlayTimer
+
+const GAME_PLAY_DURATION = 30
+
+var current_left_time = GAME_PLAY_DURATION
+var game_play_timer_left_time
 
 signal exit_to_menu
 
 func _ready():
-	pass
+	game_play_timer.set_one_shot(true)
+	game_play_timer.set_wait_time(GAME_PLAY_DURATION)
+	
+
+func _process(delta):
+	game_play_timer_left_time = ceil(game_play_timer.get_time_left())
+	if current_left_time != game_play_timer_left_time:
+		current_left_time = game_play_timer_left_time
 
 func set_player(player):
 	add_child(player, true)
@@ -13,5 +27,9 @@ func set_player(player):
 	$ARVROrigin/LeftTouchController/TimerWatch.set_visible(true)
 	
 func _on_VRController_x_button_pressed():
-	emit_signal("exit_to_menu")	
+	emit_signal("exit_to_menu")
 	
+
+func _on_StartGameCountDownTimer_timeout():
+	
+	game_play_timer.start()
