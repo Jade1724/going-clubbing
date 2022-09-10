e74836081e151e56b6be056e162c28eea7196946
Jade1724 <haruka.i1724@gmail.com>
2022-09-10 18:38:16 +1200

    Made TimerWatch label updating with game play timer count down

--- Scripts/GamePlay/GamePlay.gd
+++ Scripts/GamePlay/GamePlay.gd
@@ -7,6 +7,7 @@ const GAME_PLAY_DURATION = 30
 
 var current_left_time = GAME_PLAY_DURATION
 var game_play_timer_left_time
+var timer_watch
 
 signal exit_to_menu
 
@@ -19,17 +20,18 @@ func _process(delta):
 	game_play_timer_left_time = ceil(game_play_timer.get_time_left())
 	if current_left_time != game_play_timer_left_time:
 		current_left_time = game_play_timer_left_time
+		timer_watch.set_left_time(current_left_time)
 
 func set_player(player):
 	add_child(player, true)
 	player.set_name("ARVROrigin")
 	$ARVROrigin/LeftTouchController.connect("x_button_pressed", self, "_on_VRController_x_button_pressed")
-	$ARVROrigin/LeftTouchController/TimerWatch.set_visible(true)
+	timer_watch = $ARVROrigin/LeftTouchController/TimerWatch
+	timer_watch.set_visible(true)
 	
 func _on_VRController_x_button_pressed():
 	emit_signal("exit_to_menu")
 	
 
 func _on_StartGameCountDownTimer_timeout():
-	
 	game_play_timer.start()
---
+++ Scripts/GamePlay/TimerWatch.gd
@@ -0,0 +1,5 @@
+extends MeshInstance
+
+
+func set_left_time(time):
+	$Viewport/Label.set_text(str(time))
